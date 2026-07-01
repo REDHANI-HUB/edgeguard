3a2afa4b97ec5c743a97b2d6dd5e6abbcf3d65db
REDHANI C <chelladurairedhani@gmail.com>
2026-07-01 23:31:02 +0530

    Initial EdgeGuard backend setup

---
+++ src/main/java/com/example/edgeguard/EdgeguardApplication.java
@@ -0,0 +1,13 @@
+package com.example.edgeguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeguardApplication.class, args);
+    }
+
+}
